6e3407e0a04543af1c101c244d26ee2c3b39e213
jcampos-94 <jcampos.94@hotmail.com>
2026-04-02 18:15:34 -0500

    firebase test succesfull

--- loreforge/views.py
+++ loreforge/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render, redirect
 from django.shortcuts import get_object_or_404
 from .models import Faction, Character
 from .forms import FactionForm, CharacterForm
+from .firebase_config import db
 
 
 # Main page (landing view)
